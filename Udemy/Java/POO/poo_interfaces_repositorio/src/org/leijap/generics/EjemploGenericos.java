cdbdf6de33516efcc51aa1787c948873d1c54a2d
Leixhik <eduardo.leijapl@uanl.edu.mx>
2024-07-29 17:27:44 -0600

    ---v.1.1.4: Creacion de un tipo Generico extends Comparable que permite comparár entre tres elementos al mayor de ellos, utilizando ejemplos de int, float y string.---

--- Udemy/Java/POO/poo_interfaces_repositorio/src/org/leijap/generics/EjemploGenericos.java
+++ Udemy/Java/POO/poo_interfaces_repositorio/src/org/leijap/generics/EjemploGenericos.java
@@ -39,6 +39,10 @@ public class EjemploGenericos {
         imprimirClientes(clientesLista);
         imprimirClientes(clientePremiumList);
 
+        System.out.println("Máximo de 1, 9 y 4 es: " + maximo(1, 9, 4));
+        System.out.println("Máximo de 3.9, 11.6, 7.8 es: " + maximo(3.9, 11.6, 7.8));
+        System.out.println("Máximo de zanahoria, arándanosn manzana es: " + maximo("zanahoria", "arándanos", "manzana"));
+
     }
 
     public static <T> List<T> fromArrayToList(T[] c){
@@ -60,5 +64,16 @@ public class EjemploGenericos {
 
     public static void imprimirClientes(List<? extends Cliente> clientes){
         clientes.forEach(System.out::println);/*La expresión lambda System.out::println proporciona una forma concisa de imprimir cada elemento.*/
+    }
+
+    public static <T extends Comparable<T>> T maximo(T a, T b, T c){ //Comparar a,b,c para ver cuál es el mayor.
+        T max = a; //a será el mayor al inicio y será el comparado con los demás.
+        if (b.compareTo(max) > 0){ //Se puede utilizar compareTo gracias al extends Comparable.
+            max = b;
+        }
+        if (c.compareTo(max) > 0){
+            max = c;
+        }
+        return max;
     }
 }
