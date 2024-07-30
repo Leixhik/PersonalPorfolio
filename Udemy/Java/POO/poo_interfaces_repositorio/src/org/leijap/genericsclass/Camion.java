0ff9779df34a2a7fc3348c104fa12d1bed262c60
Rodolfo Leija <leija_26010@hotmail.com>
2024-07-30 14:55:03 -0600

    v.1.1.5 ---- Creación de un package nuevo, una Clase Genérica (Camion), y creación de varias clases normales para ejemplos. ----

---
+++ Udemy/Java/POO/poo_interfaces_repositorio/src/org/leijap/genericsclass/Camion.java
@@ -0,0 +1,30 @@
+package org.leijap.genericsclass;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Camion implements Iterable {
+
+    private List objetos;
+    private int max;
+
+    public Camion(int max) {
+        this.max = max;
+        this.objetos = new ArrayList();
+    }
+
+    public void add(Object objeto) {
+        if (this.objetos.size() >= max) {
+            this.objetos.add(objeto);
+        } else {
+            throw new RuntimeException("no hay más espacio."); //Manejo de error.
+        }
+    }
+
+    @Override
+    public Iterator iterator() {
+        return this.objetos.iterator();
+    }
+}
---
+++ Udemy/Java/POO/poo_interfaces_repositorio/src/org/leijap/genericsclass/Maquinaria.java
@@ -0,0 +1,13 @@
+package org.leijap.genericsclass;
+
+public class Maquinaria {
+    private String tipo;
+
+    public Maquinaria(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+}
