2217264bfd468dc18e11318b58b2b34ebf09348d
Leixhik <eduardo.leijapl@uanl.edu.mx>
2024-08-01 14:53:34 -0600

    v.1.1.7:
    Se han modificado las clase main para ser Genéricas e imprimir cualquier tipo de objetos mediante métodos genéricos y de conversiones Cast.

--- Udemy/Java/POO/poo_interfaces_repositorio/src/org/leijap/genericsclass/EjemploGenericos.java
+++ Udemy/Java/POO/poo_interfaces_repositorio/src/org/leijap/genericsclass/EjemploGenericos.java
@@ -1,36 +1,49 @@
 package org.leijap.genericsclass;
+/*
+-Programación genérica: Permite crear clases y métodos que puedan trabajar con diferentes tipos de datos.
+-Tipo genérico: Representa un tipo de dato que se especificará más tarde.
+-Casting: Convertir un objeto de un tipo a otro.
+-Bucle for-each: Iterar sobre los elementos de una colección.
+-Instancia: Un objeto de una clase.*/
 
 public class EjemploGenericos {
+    public static <T> void imprimirCamion(Camion<T> camion){
+        for (T a: camion){
+            if (a instanceof Animal){
+                System.out.println(((Animal)a).getNombre() + " tipo: " + ((Animal)a).getTipo());
+            }
+            else if (a instanceof Automovil){
+                System.out.println(((Automovil)a).getMarca());
+            }
+            else if (a instanceof Maquinaria) {
+                System.out.println(((Maquinaria)a).getTipo());
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
 
-        Camion transporteCaballos = new Camion(5); //Máximo de Caballos en un Camión.
+        Camion<Animal> transporteCaballos = new Camion<>(5); //Máximo de Caballos en un Camión.
         transporteCaballos.add(new Animal("Peregrino", "Caballo"));
         transporteCaballos.add(new Animal("Grillo", "Caballo"));
         transporteCaballos.add(new Animal("Tunquen", "Caballo"));
         transporteCaballos.add(new Animal("Topocalma", "Caballo"));
         transporteCaballos.add(new Animal("Longotoma", "Caballo"));
-        for (Object o: transporteCaballos){
-            Animal a = (Animal) o; // Un cast para convertir a hacia o.
-            System.out.println(a.getNombre() + " tipo: " + a.getTipo());
-        }
 
-        Camion transMaquinas = new Camion(3);
+        imprimirCamion(transporteCaballos);
+
+
+        Camion<Maquinaria> transMaquinas = new Camion<>(3);
         transMaquinas.add(new Maquinaria("Bulldozer"));
         transMaquinas.add(new Maquinaria("Grúa Horquilla"));
         transMaquinas.add(new Maquinaria("Perforadora"));
-        for (Object o: transMaquinas){
-            Maquinaria m = (Maquinaria) o;
-            System.out.println(m.getTipo());
-        }
+        imprimirCamion(transMaquinas);
 
-        Camion transAuto = new Camion(3);
+        Camion<Automovil> transAuto = new Camion<>(3);
         transAuto.add(new Automovil("Ferrari"));
         transAuto.add(new Automovil("Lamborghini"));
         transAuto.add(new Automovil("Chevrolet"));
-        for(Object o: transAuto){
-            Automovil a = (Automovil) o;
-            System.out.println(a.getMarca());
-
-        }
+        imprimirCamion(transAuto);
     }
 }
