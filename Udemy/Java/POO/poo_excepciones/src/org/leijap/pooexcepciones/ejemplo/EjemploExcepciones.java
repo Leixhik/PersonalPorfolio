70db415b9d48c457f48d3e4a2548d46207f9bb74
Leixhik <eduardo.leijapl@uanl.edu.mx>
2024-08-19 18:07:45 -0600

    v.1.2.2
    Se ha creado un try catch con diferentes capturas de Errores, en este caso el de NumberFormar que evita que un valor sea una letra y el de Arithmetic, que no permite una division erronea o bien una operación no válida. Además el main(args) crea recursividad haciendo un bucle que no deje salir al usuario hasta que ingrese el valor correcto.

--- Udemy/Java/POO/poo_excepciones/src/org/leijap/pooexcepciones/ejemplo/EjemploExcepciones.java
+++ Udemy/Java/POO/poo_excepciones/src/org/leijap/pooexcepciones/ejemplo/EjemploExcepciones.java
@@ -1,7 +1,28 @@
 package org.leijap.pooexcepciones.ejemplo;
 
+import javax.swing.*;
+
 public class EjemploExcepciones {
     public static void main(String[] args) {
+
+        String valor = JOptionPane.showInputDialog("Ingresa un entero: ");
+        int divisor;
+
+        try {
+            divisor = Integer.parseInt(valor);
+            int division = 10/divisor;
+            System.out.println(division);
+        } catch (NumberFormatException e) {
+            System.out.println("Se detectó una excepción: Por favor ingresa un valor numérico." + e.getMessage());
+            main(args);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Capturamos la excepcion en tiempo de ejecución: " + e.getMessage());
+            main(args);
+        } finally {
+            System.out.println("Fin del programa, esto se ejecuta siempre, con o sin Exception.");
+        }
+        System.out.println("Continuamos con la ejecución");
 
     }
 }
