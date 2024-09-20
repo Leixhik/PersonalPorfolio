3d30e409521720d1f2f9f2b284035dc8c6a6a9b1
Rodolfo Leija <leija_26010@hotmail.com>
2024-09-20 00:34:45 -0600

    v1.2.6.2
    Utilicé un JOption.showMessage para mostrar el mensaje de bienvenida.

--- MisProyectos/Ejercicio 1 - Variables y tipos de datos/src/ejercicio3_usuario_bienvenida.java
+++ MisProyectos/Ejercicio 1 - Variables y tipos de datos/src/ejercicio3_usuario_bienvenida.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+
 public class ejercicio3_usuario_bienvenida {
+
+    // Crea un programa que solicite al usuario su nombre y apellido.
+    // Después mostrar un mensaje de bienvenida con eso.
+
+    public static void main(String[] args) {
+
+        String nombre = JOptionPane.showInputDialog("Ingrese su nombre: ");
+        String apellido = JOptionPane.showInputDialog("Ingrese su nombre: ");
+
+        JOptionPane.showMessageDialog(null,"¡Bienvenido " + nombre + " " + apellido + "!");
+    }
 }
