6defb4235cb33c45be165b0be7c7bf209bb9bb30
Leixhik <eduardo.leijapl@uanl.edu.mx>
2024-10-02 15:05:17 -0600

    1.2.7.10.1
    Sólo utilicé una pruba más de rendimiento de búsqueda.

--- MisProyectos/Estructuras de Datos/Listas/Lists/src/ejercicio7_array_linked_prueba.java
+++ MisProyectos/Estructuras de Datos/Listas/Lists/src/ejercicio7_array_linked_prueba.java
@@ -20,23 +20,53 @@ public class ejercicio7_array_linked_prueba {
 
         // Medir el tiempo de agregar al inicio
         long inicio = System.nanoTime();
+        /*
         for (int i = 0; i < 1000; i++) {
             arrayList.add(0,i);
         }
+        */
         // ... agregar elementos al principio de ambas listas
         long fin = System.nanoTime();
+        /*
         System.out.println("Agregar al principio (ArrayList): " + (fin - inicio) + " ns");
+         */
 
 
         // ... repetir para LinkedList
-        inicio = System.nanoTime();
+       /*
+       inicio = System.nanoTime();
         for (int i = 0; i < 1000; i++) {
             linkedList.add(0,i);
         }
         fin = System.nanoTime();
         System.out.println("Agregar al principio (LinkedList): " + (fin - inicio) + " ns");
+        */
 
         // ... repetir para otras operaciones (agregar al final, en el medio, eliminar, acceder, iterar).
 
+        // Aquí estaré haciendo las pruebas
+
+        /* 1. Medir el tiempo de buscar un elemento por valor*/
+        int elementoaBuscar = 38;
+        inicio = System.nanoTime();
+        // ... buscar el elemento en arrayList
+        fin = System.nanoTime();
+        System.out.println("Buscar por valor (ArrayList): " + (fin - inicio) + " ns");
+
+        if (arrayList.contains(elementoaBuscar)) {
+            System.out.println("El elemento " + elementoaBuscar + " existe en el array");
+        } else {
+            System.out.println("La lista no contiene el elemento " + elementoaBuscar + " en el array");
+        }
+
+        System.out.println("----------------------");
+        inicio = System.nanoTime();
+        fin = System.nanoTime();
+        System.out.println("Buscar por valor (LinkedList): " + (fin - inicio) + " ns");
+        if (linkedList.contains(elementoaBuscar)) {
+            System.out.println("El elemento " + elementoaBuscar + " existe en el array");
+        } else {
+            System.out.println("El elemento " + elementoaBuscar + " existe en el array");
+        }
     }
 }
