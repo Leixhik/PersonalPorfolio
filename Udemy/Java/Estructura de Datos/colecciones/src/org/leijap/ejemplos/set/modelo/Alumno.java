a97f7cb1bf28c7f5a8c9fd0309d4967a50a9171d
Rodolfo Leija <leija_26010@hotmail.com>
2024-10-16 20:17:37 -0600

    1.2.8.5
    Hoy he conocido la diferencia entre un TreeSet y HashSet, sus diferentes funciones y diferentes maneras de implementarlo.

--- Udemy/Java/Estructura de Datos/colecciones/src/org/leijap/ejemplos/set/modelo/Alumno.java
+++ Udemy/Java/Estructura de Datos/colecciones/src/org/leijap/ejemplos/set/modelo/Alumno.java
@@ -1,5 +1,7 @@
 package org.leijap.ejemplos.set.modelo;
 
+import java.util.Objects;
+
 public class Alumno implements Comparable<Alumno>{
     private String nombre;
     private Integer nota;
@@ -53,5 +55,18 @@ public class Alumno implements Comparable<Alumno>{
             return 0;
         }
         return this.nota.compareTo(a.nota);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;/*verifica si se están comparando el mismo objeto.*/
+        if (o == null || getClass() != o.getClass()) return false; /*asegura que el objeto o no sea nulo y que sea de la misma clase que el objeto actual.*/
+        Alumno alumno = (Alumno) o;
+        return Objects.equals(nombre, alumno.nombre)/*&& Objects.equals(nota, alumno.nota)*/;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre/*, nota*/); //Criterios que utilizará el equals para comparar.
     }
 }
--- Udemy/Java/Estructura de Datos/colecciones/src/org/leijap/ejemplos/set/set/EjemploHashSetUnicidad.java
+++ Udemy/Java/Estructura de Datos/colecciones/src/org/leijap/ejemplos/set/set/EjemploHashSetUnicidad.java
@@ -2,20 +2,22 @@ package org.leijap.ejemplos.set.set;
 
 import org.leijap.ejemplos.set.modelo.Alumno;
 
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
-public class EjemploTreeSetComparable {
+public class EjemploHashSetUnicidad {
     public static void main(String[] args) {
 
-        Set<Alumno> sa = new TreeSet<>((a, b) -> b.getNombre().compareTo(a.getNombre()));
+        Set<Alumno> sa = new HashSet<>();
         sa.add(new Alumno("Pato", 5));
         sa.add(new Alumno("Cata", 6));
         sa.add(new Alumno("Luci", 4));
         sa.add(new Alumno("Jano", 7));
         sa.add(new Alumno("Andres", 3));
         sa.add(new Alumno("Zeus", 2));
-        sa.add(new Alumno("Lucas", 8));
+        sa.add(new Alumno("Zeus", 2));
+        sa.add(new Alumno("Lucas", 2));
+        sa.add(new Alumno("Lucas", 3));
 
         System.out.println(sa);
     }
