0a85267d3bd6f3637cedff0b1fd05783ea2391bc
Leixhik <eduardo.leijapl@uanl.edu.mx>
2024-10-28 16:03:55 -0600

    v.1.2.8.4
    Se crearon iteradores para las list o set que se hagan, son sencillos, pues sólo se utiliza el .getNombre();

--- Udemy/Java/Estructura de Datos/colecciones/src/org/leijap/ejemplos/set/set/EjemploHashSetUnicidad.java
+++ Udemy/Java/Estructura de Datos/colecciones/src/org/leijap/ejemplos/set/set/EjemploHashSetUnicidad.java
@@ -3,6 +3,7 @@ package org.leijap.ejemplos.set.set;
 import org.leijap.ejemplos.set.modelo.Alumno;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class EjemploHashSetUnicidad {
@@ -19,6 +20,28 @@ public class EjemploHashSetUnicidad {
         sa.add(new Alumno("Lucas", 2));
         sa.add(new Alumno("Lucas", 3));
 
-        System.out.println(sa);
+        //System.out.println(sa);
+
+        System.out.println("Iterando usando un for each: ");
+        for(Alumno a: sa){
+            System.out.println(a.getNombre());
+        }
+
+        System.out.println("Iterando usando while e iterator: ");
+        Iterator<Alumno> it = sa.iterator();
+        while(it.hasNext()){
+            Alumno a = it.next();
+            System.out.println(a.getNombre());
+        }
+
+        System.out.println("Iterando usando Stream forEach");
+        sa.forEach(a -> System.out.println(a.getNombre()));
+
+        //En caso de ser un List
+        /*for(int i = 0; i < sa.size(); i++){
+            Alumno a = sa.get(i);
+            System.out.println(sa.getNombre())
+        }
+        * */
     }
 }
