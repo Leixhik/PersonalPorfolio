9d635d3e6f1140d3f74a183d706d180bc536e262
Leixhik <eduardo.leijapl@uanl.edu.mx>
2024-11-25 16:08:10 -0600

    v. 2.0.5
    Clase: Métodos wait()  y notify().
    
    Creamos un nuevo package, para hacer una clase Panadería, en donde experimentamos con hilos sincronizados mediante wait y notify, la condición es que no puede hornearse más pan hasta que este sea consumido.

---
+++ Udemy/Java/Hilos/Threads/src/org/leijap/hilos/ejemplosync/Panaderia.java
@@ -0,0 +1,44 @@
+package org.leijap.hilos.ejemplosync;
+
+public class Panaderia {
+    private String pan;
+    private boolean disponible;
+
+    /* Mientras haya pan disponible (es decir, disponible es true), el panadero espera.
+    Esto evita que el panadero hornee nuevo pan si ya hay uno listo.*/
+    public synchronized void hornear(String masa){
+        while(disponible){
+            try {
+                wait(); //el hilo del panadero se suspende hasta que otro hilo lo notifique.
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        this.pan = masa;
+        System.out.println("Panadero hornea: " + this.pan);
+        this.disponible = true; // Pan disponible
+        notify(); /*El panadero notifica al cliente de que hay nuevo pan disponible,
+                    permitiendo que el cliente continúe con su tarea*/
+    }
+
+    //Mientras no haya pan disponible (es decir, disponible es false), el cliente espera.
+    public synchronized String consumir(){
+        while(!disponible){
+            try {
+                wait(); /*El cliente se suspende hasta que el panadero notifique que hay nuevo pan.*/
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        /*Una vez notificado, el cliente consume el pan y establece disponible en false.*/
+        System.out.println("Cliente consume: " + this.pan);
+        this.disponible = false; // Pan no disponible
+        notify();
+        return pan;
+
+        /* ↑↑ Los métodos hornear y consumir están sincronizados, lo que garantiza que solo un hilo
+        (ya sea el panadero o el cliente) pueda acceder a las variables compartidas pan
+        y disponible en un momento dado*/
+    }
+}
