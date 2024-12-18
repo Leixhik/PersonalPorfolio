e5178968b66901d230fb26563b5c60dae487023f
Rodolfo Leija <leija_26010@hotmail.com>
2024-12-18 17:11:56 -0600

    v.2.0.16
    Clase: Tareas programadas que se repiten cada cierto tiempo
    
    Se copió la clase anterior, y utilizamos diferentes objetos con métodos específicos para ver el úso de las tareas programadas, en este caso, usando un .scheduleAtFixedRate para tener tiempos precisos.

--- Udemy/Java/Hilos/Threads/src/org/leijap/hilos/ejemploexecutor/EjemploScheduledExecutorServicePeriodo.java
+++ Udemy/Java/Hilos/Threads/src/org/leijap/hilos/ejemploexecutor/EjemploScheduledExecutorServicePeriodo.java
@@ -1,24 +1,40 @@
 package org.leijap.hilos.ejemploexecutor;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class EjemploScheduledExecutorServicePeriodo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
 
         System.out.println("Alguna tarea en el main ...");
 
-        executor.schedule(() -> {
-            try {
-                TimeUnit.MILLISECONDS.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();            }
-            System.out.println("Hola Mundo tarea ...");
-        }, 2000 , TimeUnit.MILLISECONDS);
+        // Instancia de cuenta regresiva
+//        CountDownLatch lock = new CountDownLatch(5);
 
+        AtomicInteger contador = new AtomicInteger(5);
+
+        Future<?> future =  executor.scheduleAtFixedRate(() -> {
+            System.out.println("Hola mundo tarea...");
+            try {
+                TimeUnit.MILLISECONDS.sleep(1000); // Simular una tarea que tarda 1 segundo
+//                lock.countDown();
+                contador.getAndDecrement();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, 1000 ,2000, TimeUnit.MILLISECONDS);
+
+//        lock.await();
+//        future.cancel(true);
+//        TimeUnit.SECONDS.sleep(10); // La tarea se ejecuta durante 10 segundos y después se apaga
+        while(contador.get() >= 0){
+            if (contador.get() == 0) {
+                future.cancel(true);
+                contador.getAndDecrement();
+            }
+        }
         System.out.println("Alguna otra tarea en el main ...");
         executor.shutdown();
     }
